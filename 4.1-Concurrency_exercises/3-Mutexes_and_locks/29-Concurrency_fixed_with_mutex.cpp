a27f36c8c04be113e71fbef6883ba67a6bbe210b
Tiago Sousa <t.l.sousa@gmail.com>
2025-04-15 19:39:04 +0100

    Add more concurrency exercises

---
+++ 4.1-Concurrency_exercises/3-Mutexes_and_locks/29-Concurrency_fixed_with_mutex.cpp
@@ -0,0 +1,66 @@
+#include <iostream>
+#include <thread>
+#include <vector>
+#include <future>
+#include <mutex>
+#include<algorithm>
+
+class Vehicle
+{
+public:
+    Vehicle(int id) : _id(id) {}
+    int getID() {
+        return _id;
+    }
+private:
+    int _id;
+};
+
+class WaitingVehicles
+{
+public:
+    WaitingVehicles() {}
+
+    // getters / setters
+    void printSize()
+    {
+        _mutex.lock();
+        std::cout << "#vehicles = " << _vehicles.size() << std::endl;
+        _mutex.unlock();
+    }
+
+    // typical behaviour methods
+    void pushBack(Vehicle &&v)
+    {
+        _mutex.lock();
+        _vehicles.emplace_back(std::move(v));
+        std::this_thread::sleep_for(std::chrono::milliseconds(10));
+        _mutex.unlock();
+        return;
+    }
+
+private:
+    std::vector<Vehicle> _vehicles; // list of all vehicles waiting to enter this intersection
+    std::timed_mutex _mutex;
+};
+
+int main()
+{
+    std::shared_ptr<WaitingVehicles> queue(new WaitingVehicles); 
+    std::vector<std::future<void>> futures;
+    for (int i = 0; i < 1000; ++i)
+    {
+        futures.emplace_back(std::async(std::launch::async, [queue, i]() {
+            Vehicle v(i);
+            queue->pushBack(std::move(v));
+        }));
+    }
+
+    std::for_each(futures.begin(), futures.end(), [](std::future<void> &ftr) {
+        ftr.wait();
+    });
+
+    queue->printSize();
+
+    return 0;
+}
---
+++ 4.1-Concurrency_exercises/4-Conditions_variables_and_message_queues/41-solution_condition_variable.cpp
@@ -0,0 +1,77 @@
+#include <iostream>
+#include <thread>
+#include <vector>
+#include <future>
+#include <mutex>
+#include <algorithm>
+#include <deque>
+
+template <class T> 
+class MessageQueue
+{
+public:
+    MessageQueue() {}
+
+    T receive()
+    {
+        // perform vector modification under the lock
+        std::unique_lock<std::mutex> uLock(_mutex);
+        _cond.wait(uLock, [this] { return !_messages.empty(); }); // pass unique lock to condition variable
+
+        // remove last vector element from queue
+        T msg = std::move(_messages.back());
+        _messages.pop_back();
+
+        return msg; // will not be copied due to return value optimization (RVO) in C++
+    }
+
+    void send(T &&msg)
+    {
+        // simulate some work
+        std::this_thread::sleep_for(std::chrono::milliseconds(100));
+
+        // perform vector modification under the lock
+        std::lock_guard<std::mutex> uLock(_mutex);
+
+        // add vector to queue
+        std::cout << "   Message #" << msg << " will be added to the queue" << std::endl;
+        _messages.push_back(std::move(msg));
+        _cond.notify_one(); // notify client after pushing new Vehicle into vector
+    }
+
+private:
+    std::mutex _mutex;
+    std::condition_variable _cond;
+    std::deque<T> _messages; // list of all vehicles waiting to enter this intersection
+};
+
+int main()
+{
+    // create monitor object as a shared pointer to enable access by multiple threads
+    std::shared_ptr<MessageQueue<int>> queue(new MessageQueue<int>);
+
+    std::cout << "Spawning threads..." << std::endl;
+    std::vector<std::future<void>> futures;
+    for (int i = 0; i < 10; ++i)
+    {
+        // create a new Vehicle instance and move it into the queue
+        int message = i;
+        futures.emplace_back(std::async(std::launch::async, &MessageQueue<int>::send, queue, std::move(message)));
+    }
+
+    std::cout << "Collecting results..." << std::endl;
+    while (true)
+    {
+        // popBack wakes up when a new element is available in the queue
+        int message = queue->receive();
+        std::cout << "   Message #" << message << " has been removed from the queue" << std::endl;
+    }
+
+    std::for_each(futures.begin(), futures.end(), [](std::future<void> &ftr) {
+        ftr.wait();
+    });
+
+    std::cout << "Finished!" << std::endl;
+
+    return 0;
+} 
